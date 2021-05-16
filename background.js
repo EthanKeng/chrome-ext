cff50a38be074cb7803af73fa5abdc06750f2dc0
EthanKeng <36765774+EthanKeng@users.noreply.github.com>
2021-05-16 16:35:42 +0900

    Add files via upload

---
+++ background.js
@@ -0,0 +1,10 @@
+console.log("bg is running")
+chrome.browserAction.onClicked.addListener(buttonClicked);
+
+function buttonClicked(tab) {
+  // The user clicked the button!
+  // 'tab' is an object with information about the current open tab
+  console.log("clicked!")
+  console.log(tab)	
+
+}
